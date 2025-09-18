0cfa3f3a554c465ea70478ae1628d9b32743c7f7
Ubuntu <ubuntu@ip-172-31-13-215.ap-southeast-2.compute.internal>
2025-09-18 08:37:47 +0000

    Initial commit

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, SonarQube!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;  // Simple method to check in SonarQube
+    }
+}
+
